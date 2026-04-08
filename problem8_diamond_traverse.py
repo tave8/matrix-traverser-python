4937488f37bd6bc866c88ee3c838d1bed36832a3
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-08 23:22:42 +0200

    solved problem diamond traverse

---
+++ problem8_diamond_traverse.py
@@ -0,0 +1,86 @@
+"""
+PROBLEM: Traverse the matrix in diamond traverse.
+
+
+"""
+
+from MatrixTraverser import MatrixTraverser, MatrixTraverserCallbackManager, MatrixTraverserStateManager, Coordinate, Move
+
+
+matrix = [
+    [1,    2,    6,   7,   15],
+    [3,    5,    8,   14,  16],
+    [4,    9,    13,  17,  22],
+    [10,   12,   18,  21,  23],
+    [11,   19,   20,  24,  25]
+]
+
+state = {
+    "values": []
+}
+
+
+def beforeFirstVisitCallback(mt: MatrixTraverser, 
+                             prevCoordinate: Coordinate, 
+                             currCoordinate: Coordinate,
+                             prevMove: Move):
+    if currCoordinate.isStart:
+        print(f"START: {mt.getAtCoordinate(currCoordinate)} ({prevMove.name})")
+    else:
+        print(f"FROM {mt.getAtCoordinate(prevCoordinate)} TO {mt.getAtCoordinate(currCoordinate)} ({prevMove.name})")
+
+    # print(mt.stateManager.stats)
+
+
+def getNextMovesCallback(mt: MatrixTraverser, 
+                         prevCoordinate: Coordinate, 
+                         currCoordinate: Coordinate,
+                         prevMove: Move):
+
+   # previous move: next moves 
+   moves = {
+        Move._BEFORE_START: [Move.RIGHT],
+        Move.DOWN: [Move.DIAGONAL_UP_RIGHT],
+        Move.DIAGONAL_UP_RIGHT: [Move.DIAGONAL_UP_RIGHT, Move.RIGHT],
+        Move.RIGHT: [Move.DIAGONAL_DOWN_LEFT],
+        Move.DIAGONAL_DOWN_LEFT: [Move.DIAGONAL_DOWN_LEFT, Move.DOWN]
+    }
+   
+   return moves[prevMove]
+   
+   
+
+
+def canMoveCallback(mt: MatrixTraverser, 
+                    desiredCoordinate: Coordinate, 
+                    prevCoordinate: Coordinate, 
+                    currCoordinate: Coordinate):
+    pass    
+
+
+
+callbackMap = {
+    "canMove": canMoveCallback,
+    "getNextMoves": getNextMovesCallback,
+    "beforeFirstVisit": beforeFirstVisitCallback,
+}
+
+
+stateManager = MatrixTraverserStateManager(state)
+callbackManager = MatrixTraverserCallbackManager(callbackMap)
+
+matrixTraverser = MatrixTraverser(
+    matrix, 
+    Coordinate(0, 0),
+    callbackManager,
+    stateManager
+)
+
+# for now you cannot call the method more than once
+matrixTraverser.traverseMatrix()
+
+
+# for move, moveStats in matrixTraverser.stateManager.stats["byMove"].items():
+#     print()
+#     print(f"{move}: {moveStats}")
+#     print()
