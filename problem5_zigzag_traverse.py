b53ff8f1320373cdfa4fb022f30f61fd3a4393e3
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-09 22:16:22 +0200

    fix zigzag traverse to new updates

--- problem5_zigzag_traverse.py
+++ problem5_zigzag_traverse.py
@@ -2,7 +2,7 @@
 PROBLEM: Traverse the matrix in zigzag.
 """
 
-from MatrixTraverser import MatrixTraverser, MatrixTraverserCallbackManager, MatrixTraverserStateManager, Coordinate, Move
+from MatrixTraverser import Matrix, MatrixTraverser, Coordinate, Move
 
 
 matrix = [
@@ -22,11 +22,11 @@ def beforeFirstVisitCallback(mt: MatrixTraverser,
                              currCoordinate: Coordinate,
                              prevMove: Move):
     if currCoordinate.isStart:
-        print(f"START: {mt.getAtCoordinate(currCoordinate)} ({prevMove.name})")
+        print(f"START: {Matrix.getAtCoordinate(mt.matrix, currCoordinate)} ({prevMove.name})")
     else:
-        print(f"FROM {mt.getAtCoordinate(prevCoordinate)} TO {mt.getAtCoordinate(currCoordinate)} ({prevMove.name})")
-    # collect the current cell
-    state["values"].append(mt.getAtCoordinate(currCoordinate))
+        print(f"FROM {Matrix.getAtCoordinate(mt.matrix, prevCoordinate)} TO {Matrix.getAtCoordinate(mt.matrix, currCoordinate)} ({prevMove.name})")
+    pass
+    state["values"].append(Matrix.getAtCoordinate(mt.matrix, currCoordinate))
 
 
 def getNextMovesCallback(mt: MatrixTraverser, 
@@ -53,7 +53,8 @@ def getNextMovesCallback(mt: MatrixTraverser,
 def canMoveCallback(mt: MatrixTraverser, 
                     desiredCoordinate: Coordinate, 
                     prevCoordinate: Coordinate, 
-                    currCoordinate: Coordinate):
+                    currCoordinate: Coordinate,
+                    prevMove: Move):
     pass    
 
 
@@ -65,15 +66,15 @@ callbackMap = {
 }
 
 
-stateManager = MatrixTraverserStateManager(state)
-callbackManager = MatrixTraverserCallbackManager(callbackMap)
 
 matrixTraverser = MatrixTraverser(
     matrix, 
-    Coordinate(0, 0),
-    callbackManager,
-    stateManager
+    callbackMap,
+    state
 )
 
 # for now you cannot call the method more than once
-matrixTraverser.traverseMatrix()
+matrixTraverser.traverseMatrix(
+    Coordinate.generateIsStartCoord(0, 0), 
+    Coordinate.generateIsBeforeStartCoord()
+)
