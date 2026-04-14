22f6b39421e49a999b506a5a2672aefb8b6c6c2d
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-04-14 19:58:11 +0200

    test line & find k ancestors: 2 tests passed

--- tests/examples/line/test_line.py
+++ tests/examples/line/test_line.py
@@ -133,3 +133,130 @@ def test_up():
     assert Matrix.getAtCoordinate(matrix, ancestors[5].coord) == 6
 
 
+def test_right():
+
+    matrix = [
+        [1, 10, -2, 3, -22, 8],
+    ]
+
+    startCoord = Coordinate(
+        Matrix.getFirstRow(),
+        Matrix.getFirstCol(),
+    )
+
+    targetValue = 8
+    move = Move.RIGHT
+
+    dataToTest = makeAndRunLinePattern(matrix, startCoord, move)
+
+    (nodeFound, ancestors) = MatrixTree.findOneByValueFrom(
+        dataToTest.patternTraverser.matrixTree,
+        targetValue,
+        matrix
+    )
+
+
+    # assert that the node found has
+    # the value we were looking for
+    assert isinstance(nodeFound, MatrixTree)
+    # assert that the number of ancestors is correct
+    # print(ancestors)
+    assert len(ancestors) == 5
+    # # the target value is equal to the value
+    # # at the coordinate in the node that was found
+    # assert targetValue == Matrix.getAtCoordinate(matrix, nodeFound.coord)
+    # # assert equality for each ancestor's value
+    assert Matrix.getAtCoordinate(matrix, ancestors[0].coord) == 1
+    assert Matrix.getAtCoordinate(matrix, ancestors[1].coord) == 10
+    assert Matrix.getAtCoordinate(matrix, ancestors[2].coord) == -2
+    assert Matrix.getAtCoordinate(matrix, ancestors[3].coord) == 3
+    assert Matrix.getAtCoordinate(matrix, ancestors[4].coord) == -22
+
+
+
+def test_right():
+
+    matrix = [
+        [1, 10, -2, 3, -22, 8],
+    ]
+
+    startCoord = Coordinate(
+        Matrix.getFirstRow(),
+        Matrix.getFirstCol(),
+    )
+
+    targetValue = 8
+    move = Move.RIGHT
+
+    dataToTest = makeAndRunLinePattern(matrix, startCoord, move)
+
+    (nodeFound, ancestors) = MatrixTree.findOneByValueFrom(
+        dataToTest.patternTraverser.matrixTree,
+        targetValue,
+        matrix
+    )
+
+
+    # assert that the node found has
+    # the value we were looking for
+    assert isinstance(nodeFound, MatrixTree)
+    # assert that the number of ancestors is correct
+    # print(ancestors)
+    assert len(ancestors) == 5
+    # # the target value is equal to the value
+    # # at the coordinate in the node that was found
+    # assert targetValue == Matrix.getAtCoordinate(matrix, nodeFound.coord)
+    # # assert equality for each ancestor's value
+    assert Matrix.getAtCoordinate(matrix, ancestors[0].coord) == 1
+    assert Matrix.getAtCoordinate(matrix, ancestors[1].coord) == 10
+    assert Matrix.getAtCoordinate(matrix, ancestors[2].coord) == -2
+    assert Matrix.getAtCoordinate(matrix, ancestors[3].coord) == 3
+    assert Matrix.getAtCoordinate(matrix, ancestors[4].coord) == -22
+
+
+
+
+def test_diagonal_up_left():
+
+    matrix = [
+        [10, 10, -2,  3,  -22,  8],
+        [6, 10, -2,  3,  -22,  8],
+        [1,  5, -2,  3,  -22,  8],
+        [1, 10, -4,  3,  -22,  8],
+        [1, 10, -2, -3,  -22,  8],
+        [1, 10, -2,  3,   -2,  8],
+        [1, 10, -2,  3,  -22,  1],
+    ]
+
+    startCoord = Coordinate(
+        Matrix.getLastRow(matrix),
+        Matrix.getLastCol(matrix),
+    )
+
+    targetValue = 6
+    move = Move.DIAGONAL_UP_LEFT
+
+    dataToTest = makeAndRunLinePattern(matrix, startCoord, move)
+
+    (nodeFound, ancestors) = MatrixTree.findOneByValueFrom(
+        dataToTest.patternTraverser.matrixTree,
+        targetValue,
+        matrix
+    )
+
+
+    # assert that the node found has
+    # the value we were looking for
+    assert isinstance(nodeFound, MatrixTree)
+    # assert that the number of ancestors is correct
+    # print(ancestors)
+    assert len(ancestors) == 5
+    # # the target value is equal to the value
+    # # at the coordinate in the node that was found
+    # assert targetValue == Matrix.getAtCoordinate(matrix, nodeFound.coord)
+    # # assert equality for each ancestor's value
+    assert Matrix.getAtCoordinate(matrix, ancestors[0].coord) == 1
+    assert Matrix.getAtCoordinate(matrix, ancestors[1].coord) == -2
+    assert Matrix.getAtCoordinate(matrix, ancestors[2].coord) == -3
+    assert Matrix.getAtCoordinate(matrix, ancestors[3].coord) == -4
+    assert Matrix.getAtCoordinate(matrix, ancestors[4].coord) == 5
